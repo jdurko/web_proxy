5237c66a9b20de5529ace86f2f5fd209c196975f
kodjo Komedja <fabricenub@yahoo.fr>
2015-04-23 10:54:46 -0500

    report-first and read-me change and code update
    please justin look at report some part need to be filled by you and take
    a look at code to do it. if u have quesion ask me

--- ProxyDK.cpp
+++ ProxyDK.cpp
@@ -165,25 +165,13 @@ while(1){
      while (read(msg_size,line,1)>0)
     {
      j=write(sock,line,1);
-     // printf(" CACHED write ERRNO %d\n", errno);
-     // printf(" CACHED DATA %s\n",line);
+
 
      }
 
   infile.close();
   j=close(msg_size);
- // close(sock);
- // free(sock_desc);
- // exit(1);
-			// get file size using buffer's members
-//			std::size_t size = pbuf->pubseekoff(0, infile.end, infile.in);
-//			pbuf->pubseekpos(0, infile.in);
-//			// get file data
-//			pbuf->sgetn(buffer, size);
-//			infile.close();
-//			if (send(sock, buffer,size , 0) < 0){
-//
-//			}
+close(sock);
 		}
 		//file not in cache
 		if (exist == false){
@@ -195,18 +183,16 @@ while(1){
 //		}
         if(strcmp(it->second.c_str(),weblink)==0){
 			checkin==true;
-			weblink="www.cnet.edu"
-			// break;
+			string block="blocking.text";
+			strcpy(weblink,block.c_str());
 			msg_size= open("blocking.text", O_RDWR);
            printf(" CACHED OPEN ERRNO %d\n", errno);
      while (read(msg_size,line,1)>0)
     {
      j=write(sock,line,1);
-     // printf(" CACHED write ERRNO %d\n", errno);
-     // printf(" CACHED DATA %s\n",line);
 
      }
-     //close(sock);
+     close(sock);
      break;
 		}
 
@@ -255,115 +241,9 @@ int PORT = 80;
     }
 
 
-			/* create socket for sending data */
-//		sock_send = socket(AF_INET, SOCK_STREAM, 0);
-//			if (sock_send < 0) {
-//				printf("socket() failed\n");
-//				exit(0);
-//			}
-//
-//			/* create socket address structure to connect to */
-//			memset(&addr_send, 0, sizeof(addr_send)); /* zero out structure */
-//			addr_send.sin_family = AF_INET; /* address family */
-//			if(send_len=getaddrinfo(weblink,"http",&hints,&servinfo)!=0){
-//                printf(" GET-ADDRESS-INFO %s\n",gai_strerror(send_len));
-//			}
-//			for(p=servinfo;p!=NULL ;p=p->ai_next){
-//                add_send=(struct sockaddr_in )p->ai_addr;
-//                strcpy()
-//
-//			}
-//			 addr_send.sin_addr.s_addr = inet_addr(weblink);
-//            addr_send.sin_port = htons((unsigned short)HTTP_PORT);
-//
-//			//addr_send.sin_addr.s_addr = htonl(INADDR_ANY);
-//			std::string lookup=weblink;
-//			//lookup.replace(0,4,"GET http://" );
-//			//lookup=""+lookup+" HTTP/1.0\n\n";
-//			//lookup.replace(0,4,"");
-//			printf(" TEST LOOKUP %s\n", lookup.c_str());
-		//	addr_send.sin_addr.s_addr = inet_addr(lookup.c_str());
-			//addr_send.sin_addr.s_addr = inet_addr(SERVER_IP);
-			//addr_send.sin_port = htons((unsigned short)HTTP_PORT);
 
-/*int statu;
-  struct addrinfo host_info;       // The struct that getaddrinfo() fills up with data.
-  struct addrinfo *host_info_list; // Pointer to the to the linked list of host_info's.
-
-  // The MAN page of getaddrinfo() states "All  the other fields in the structure pointed
-  // to by hints must contain either 0 or a null pointer, as appropriate." When a struct
-  // is created in C++, it will be given a block of memory. This memory is not necessary
-  // empty. Therefor we use the memset function to make sure all fields are NULL.
-  memset(&host_info, 0, sizeof host_info);
-
-  std::cout << "Setting up the structs..."  << std::endl;
-
-  host_info.ai_family =  AF_INET;     // IP version not specified. Can be both.
-  host_info.ai_socktype = SOCK_STREAM; // Use SOCK_STREAM for TCP or SOCK_DGRAM for UDP.
-
-  // Now fill up the linked list of host_info structs with google's address information.
-  statu = getaddrinfo(weblink, "80", &host_info, &host_info_list);
-  // getaddrinfo returns 0 on succes, or some other value when an error occured.
-  // (translated into human readable text by the gai_gai_strerror function).
-  if (statu != 0)  std::cout << "getaddrinfo error" << gai_strerror(statu) ;
-  std::cout << "Creating a socket..."  << std::endl;
-int socketfd ; // The socket descripter
-socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype,
-host_info_list->ai_protocol);
-if (socketfd == -1)  std::cout << "socket error " ;
-std::cout << "Connect()ing..."  << std::endl;
-statu = connect(socketfd, host_info_list->ai_addr, host_info_list->ai_addrlen);
-if (statu == -1)  std::cout << "connect error" ;
- printf(" STATU %d\n", statu);
- */
-//	/*		 connect to the HTTP Socket  */
-//			i = connect(sock_send, (struct sockaddr *) &addr_send, sizeof(addr_send));
-//			if (i < 0) {
-//                    printf(" TEST ADDR %d\n", errno);
-//				printf("connect() failed\n");
-//				close(sock_send);
-//				exit(0);
-//			}
-//Check Port number status
-//	int statu = bind(text, (struct sockaddr *) &addr_send,
-//		sizeof(addr_send));
-//	if (statu < 0) {
-//		perror("bind()text failed");
-//		close(text);
-//		exit(1);
-//	}
-//	// Listen port number
-//	statu = listen(text, 5);
-//	if (statu < 0) {
-//		perror("listen()text failed");
-//		close(text);
-//		exit(1);
-//	}
-//			std::string word=weblink;
-//			//word.replace(0,4,"GET http://www." );
-//			//word=""+word+" HTTP/1.0\n\n";
-//			//word.replace(0,4,"GET http://www." );
-//			word.replace(0,4,"GET /www." );
-//		   //word=word+" HTTP/1.1\r\nAccept: text/html, application/xhtml+xml, */*\r\nConnection: Keep-Alive\r\n\r\n";
-//			word="GET / HTTP/1.1\r\n\r\n";
-//			const char *msg =word.c_str();
-//			//std::cout << word ;
-//			 printf(" WEBLINK THING FIRST11 %s\n", msg);
-//			 std::string response;
-//			if (send(sfd, msg,sizeof(msg) , 0) < 0){
-//                printf(" TEST SEND ERRNO %d\n", errno);
-//			}
 //			 // allocate memory for file content
-//            printf(" SENT %d\n",1);
-//			char incoming_data_buffer[9000];
-//			if(recv(sfd,incoming_data_buffer,sizeof(incoming_data_buffer), 0)<=0){
-//         printf(" TEST RECV ERRNO %d\n", errno);
-//    }
-// printf(" WEBLINK THING RESPONSE %s\n", incoming_data_buffer);
 
-//			if(write(sock_send,word,sizeof(word))==-1){
-
-		//	}
   stringstream ss;
     ss << "GET / HTTP/1.1\r\n"
        << "host:"<<weblink<<"\r\n"
@@ -381,20 +261,12 @@ if (statu == -1)  std::cout << "connect error" ;
  	 std::fstream filestr1;
  	// int fdB = open(url, O_CREAT | O_RDWR, 0644);
  	 filestr1.open(url,std::fstream::in | std::fstream::out | std::fstream::app);
- 	  //printf(" TEST OPEN ERRNO %d\n", errno);
-	 // write to outfile
-	// filestr1<<std::cout;
-//	  while ( read(sock_send, &cur, 1) != 0 ) {
-//       filestr1<<cur;
-//    }
-//int k=0;
+
     while ( read(sock_send,cur, 1) != 0 ) {
        // cout << cur;
          filestr1<<cur;
          text=write(sock,cur,1);
-//         if(k==10)
-//            break;
-//        k++;
+
 
     }
 // char cur[1];
@@ -402,109 +274,26 @@ printf(" TEST 1 ERRNO %d\n", errno);
 //close(sock_send);
 printf(" TEST 2 ERRNO %d\n", errno);
   close(sock);
-//string response;
-//   // ifstream myfile ("example.txt");
-//    if (filestr1.is_open())
-//    {
-////        while ( getline(filestr1, response) ){
-////          if (send(sock,response.c_str(),response.length() , 0) < 0){
-////printf(" TEST SEND ERRNO %d\n", errno);
-////			}
-//// get pointer to associated buffer object
-//		std::filebuf* pbuf1 = filestr1.rdbuf();
-//		printf(" TEST READ ERRNO %d\n", errno);
-//		// get file size using buffer's members
-//			std::size_t size = pbuf1->pubseekoff(0, filestr1.end, filestr1.in);
-//			pbuf1->pubseekpos(0, filestr1.in);
-//			// get file data
-//
-//			pbuf1->sgetn((char*)&response, size);
-//			printf(" TEST RESPONSE ERRNO %d\n", errno);
-//text=write(sock,response,size);
-//
-//        filestr1.close();
-//    }
-//    else
-//    {
-//        cout << "Unable to open file";
-//    }
-  //filestr1.write(msg,strlen(msg)) ;
- // printf(" TEST WRITE ERRNO %d\n", errno);
- // filestr1<<incoming_data_buffer;
-
-// // get pointer to associated buffer object
-//		std::filebuf* pbuf1 = filestr1.rdbuf();
-//		printf(" TEST READ ERRNO %d\n", errno);
-//		// get file size using buffer's members
-//			std::size_t size = pbuf1->pubseekoff(0, ifs.end, ifs.in);
-//			pbuf1->pubseekpos(0, ifs.in);
-//			// get file data
-//
-//			pbuf1->sgetn((char*)&response, size);
-//			printf(" TEST buffer ERRNO %d\n", errno);
-//			printf(" TEST buffer ERRNO %d\n", errno);
-//  filestr1.close();
-//freeaddrinfo(host_info_list);
-
-
- // delete &response;
- // delete &word;
-
-  // char b[1];
-//   int fdA = open(url,O_RDWR);
-//    int n_char=0;
-   //Use the read system call to obtain fdA one byte at time
-  // while (read(fdA, b, 1)!=0)
-  //  {
-    // n_char=write(fdB,buffer,1);
-
-  //   }
-
-
-//  	//filestr.open (url);
-////backup = std::cout.rdbuf();     // back up cout's streambuf
-//	 psbuf1 = filestr1.rdbuf();        // get file's streambuf
-//  	std::cout.rdbuf(psbuf1);         // assign streambuf to cout
-//////  iterate over the map
-
-
 
 			//send a connection request
-			//bytes_sent = send(sock_send, buff, BUF_SIZE, 0);
-			//if (bytes_sent < 0){
-
-			//}
-
-		//	int fdB = open("/home/knk0055/public_html/text51.txt", O_CREAT | O_RDWR, 0644);
 
 }
 if(checkin==true){
-//char block[]="blocking.text";
+//   "blocking web content";
 printf(" Blocking website \n");
    msg_size= open("blocking.text", O_RDWR);
            printf(" CACHED OPEN ERRNO %d\n", errno);
      while (read(msg_size,line,1)>0)
     {
      j=write(sock,line,1);
-     // printf(" CACHED write ERRNO %d\n", errno);
-     // printf(" CACHED DATA %s\n",line);
+
 
      }
-    // close(sock);
-//buf1.append(token);
-//if (send(sock,&buf1, buf1.length(), 0) < 0){
-//				//TODO error  CNP_ACCONT_MESSAGE
-//        }
-//		}
+
 	}
-//	delete &exist;
-//	delete buffer;
-//	delete &checkin;
-//	delete &url;
-	//close(sock_send);
 
 close(sock);
-//free(sock_desc);
+//free sock
 threadCount++;
 }
 }
---
+++ README.txt
@@ -0,0 +1,19 @@
+PLEASE make sure you are running on  cse06.cse.unt.edu since the server 
+address has been defined in the code to run on this specific server
+
+To compile our code,open a terminal and type:   g++ -pthread  -o ProxyC Proxy_server.cpp
+
+
+
+To execute our code type:  ./ProxyC 
+
+*Case: Only when your environment paths C/C++ are not linked to your program directory
+
+Please to compile, you may specify 
+to the compiler the directory for input files 
+by using the  Path To your Directory of code, I mean  by typing
+g++ -pthread Path_To_Directory\Code Path_To_Directory\Code\Proxy_server.cpp 
+
+THANKS
+from the team
+
